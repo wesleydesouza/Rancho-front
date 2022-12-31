1db0e433219c61ed375a322c99545b6bd3f6adc9
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-12-31 16:07:29 -0300

    .

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const { resolve } = require("path");
 
-app.use("/", express.static(resolve(__dirname, "./build")));
+app.use("/", express.static(resolve(__dirname, "./tsc && vite build")));
 
 app.listen(PORT, () =>
   console.log(`Servidor iniciado em http://localhost:${PORT}`)
